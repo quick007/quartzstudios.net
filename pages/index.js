05bc8c8ed0eaf76ed999c66a272b0254a644346f
LukasmanMHdude <69743171+quick007@users.noreply.github.com>
2021-04-18 14:26:54 -0700

    Add stuff (see desc.)
    adds:
    
    - next
    - postcss
    - autoprifixer
    - tailwindcss
    - heroicons

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <div className="bg-gray-800 h-screen">
+        <h2 className="text-white">text</h2>
+      </div>
+    </>
+  )
+}
