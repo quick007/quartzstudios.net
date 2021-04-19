9e93bf85f128d11fd4fae91714b242981beb7607
LukasmanMHdude <69743171+quick007@users.noreply.github.com>
2021-04-18 20:44:01 -0700

    convert to ts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,22 @@
+
 module.exports = {
-  mode: 'jit',
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}"],
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        coolGray: {
+          700: "#2C2F33",
+          800: "#1f2124",
+          900: "#121314",
+        },
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+  colors: {},
+};
