eaa78fc8a1a8dbd050b6a534384f587bc36db8c6
LukasmanMHdude <69743171+quick007@users.noreply.github.com>
2021-04-19 20:40:40 -0700

    fix meta tag links

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -14,14 +14,14 @@ const Navbar = (props) => {
 
                 {/*Open Graph / Facebook & Discord*/}
                 <meta property="og:type" content="website" />
-                <meta property="og:url" content="https://metatags.io/" />
+                <meta property="og:url" content="https://quartzstudios.net" />
                 <meta property="og:title" content={props.title + " | Quartz Studios"} />
                 <meta property="og:description" content="Quartz Studios creates next-generation experiences in Minecraft. Experience something idk what to put here tbh. " />
                 <meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png" />
 
                 {/*Twitter*/}
                 <meta property="twitter:card" content="summary_large_image" />
-                <meta property="twitter:url" content="https://metatags.io/" />
+                <meta property="twitter:url" content="https://quartzstudios.net" />
                 <meta property="twitter:title" content={props.title + " | Quartz Studios"} />
                 <meta property="twitter:description" content="Quartz Studios creates next-generation experiences in Minecraft. Experience something idk what to put here tbh. " />
                 <meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png" />
