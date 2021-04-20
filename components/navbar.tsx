876bc2164952184d22a675399109cefcc2352f34
LukasmanMHdude <69743171+quick007@users.noreply.github.com>
2021-04-19 20:39:40 -0700

    404 & meta tags (need full website to finish)

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -7,9 +7,27 @@ const Navbar = (props) => {
         <>
             <Head>
                 <title>{props.title + " | Quartz Studios"}</title>
+
+                {/*Primary Meta Tags*/}
+                <meta name="title" content={props.title + " | Quartz Studios"} />
+                <meta name="description" content="Quartz Studios creates next-generation experiences in Minecraft. Experience something idk what to put here tbh." />
+
+                {/*Open Graph / Facebook & Discord*/}
+                <meta property="og:type" content="website" />
+                <meta property="og:url" content="https://metatags.io/" />
+                <meta property="og:title" content={props.title + " | Quartz Studios"} />
+                <meta property="og:description" content="Quartz Studios creates next-generation experiences in Minecraft. Experience something idk what to put here tbh. " />
+                <meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png" />
+
+                {/*Twitter*/}
+                <meta property="twitter:card" content="summary_large_image" />
+                <meta property="twitter:url" content="https://metatags.io/" />
+                <meta property="twitter:title" content={props.title + " | Quartz Studios"} />
+                <meta property="twitter:description" content="Quartz Studios creates next-generation experiences in Minecraft. Experience something idk what to put here tbh. " />
+                <meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png" />
             </Head>
             <nav className="bg-gray-700 flex flex-grow">
-                <img src="../public/yes.png"></img>
+                <img src="logos/main.png"></img>
             </nav>
             
         </>
