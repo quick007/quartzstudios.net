4feee94de55fa9bdaffa7f38986c684596c29d0f
LukasmanMHdude <69743171+quick007@users.noreply.github.com>
2021-04-20 23:24:59 -0700

    amooongus

--- components/servercard.tsx
+++ components/servercard.tsx
@@ -4,15 +4,17 @@ import Link from "next/link";
 const ServerCard = (props) => {
     return (
         <>
-            <div className="flex flex-col bg-gray-900 shadow-lg w-3/12 border-b-2 border-red-600">
-                <div className="relative">
-                    <img src={props.img} className=""/>
-                    <div className="absolute inset-0 bg-gradient-to-b from-[rgba(0,0,0,0)] via-[rgba(0,0,0,0)] to-gray-900" />
-                    <img className="absolute left-[10px] top-[10px] w-5/12 " src={props.logo} />
-                </div>
-                <div className="m-4">
-                    <div className="">Offline</div>
-                </div>
+            <div className="flex bg-gray-900 shadow-lg w-96 border-b-2 border-red-600">
+                <ul>
+                    <li className="relative">
+                        <img src={props.img} className=""/>
+                        <div className="absolute inset-0 bg-gradient-to-b from-[rgba(0,0,0,0)] via-[rgba(0,0,0,0)] to-gray-900" />
+                        <img className="absolute left-[10px] top-[10px] w-5/12 " src={props.logo} />
+                    </li>
+                    <li className="m-4">
+                        <div className={"rounded-full px-2 prose text-gray-200 font-semibold inline-flex bg-red-700"}>Offline</div>
+                    </li>
+                </ul>
             </div>
         </>
     );
