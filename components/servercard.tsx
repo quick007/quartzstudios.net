03dfede6a72f785db6cb64f1a9ca2b3b79b539c7
LukasmanMHdude <69743171+quick007@users.noreply.github.com>
2021-04-21 11:03:32 -0700

    boxes (mostly) done

--- components/servercard.tsx
+++ components/servercard.tsx
@@ -4,11 +4,11 @@ import Link from "next/link";
 const ServerCard = (props) => {
     return (
         <>
-            <div className="flex bg-gray-900 shadow-lg w-96 border-b-2 border-red-600">
+            <div className="flex bg-gray-900 shadow-lg w-full lg:w-96 mt-5 lg:mt-0 border-b-2 border-red-600">
                 <ul>
                     <li className="relative">
                         <img src={props.img} className=""/>
-                        <div className="absolute inset-0 bg-gradient-to-b from-[rgba(0,0,0,0)] via-[rgba(0,0,0,0)] to-gray-900" />
+                        <div className="absolute inset-0 bg-gradient-to-b from-[rgba(0,0,0,1)] via-[rgba(0,0,0,0)] to-gray-900" />
                         <img className="absolute left-[10px] top-[10px] w-5/12 " src={props.logo} />
                     </li>
                     <li className="m-4">
@@ -16,10 +16,16 @@ const ServerCard = (props) => {
                         <p className="prose-lg mb-2 text-gray-200 mx-1">{props.desc}</p>
                         <div className="relative">
                             <div className={"rounded-full px-2 prose text-gray-200 font-semibold inline-flex bg-red-700"}>Offline</div>
-                            <div className="absolute right-0 inline-flex border border-gray-500 divide-x text-gray-200 ">
-                                <div className="p-1">{props.ip}</div>
-                                <div className="p-1">Copy IP</div>
-                            </div>
+                            <button 
+                            className="group absolute right-0 inline-flex text-gray-200 rounded-full px-2 bg-gray-800 hover:bg-gray-700 focus:bg-green-600 cursor-pointer font-roboto focus:outline-none"
+                            
+                            >
+                                <div className="relative items-center justify-center">
+                                    <span  className="group-hover:invisible group-focus:invisible">{props.ip}</span>
+                                    <span className="hidden group-hover:block group-focus:hidden absolute top-0 ">Click to copy</span>
+                                    <span className="hidden group-focus:block absolute top-0 ">Copied!</span>
+                                </div>
+                            </button>
 
                         </div>
                     </li>
--- pages/index.tsx
+++ pages/index.tsx
@@ -10,11 +10,10 @@ export default function Home() {
             t
             <div className="grid-rows-3 grid-flow-col gap-4 lg:space-y-0 lg:flex lg:flex-row px-6 w-full">
               <ServerCard title="Dawn Horizens" desc="foo" img="main/demo1.png" logo="logos/dh.png" ip="dev.baezor.com" />
-              <ServerCard title="ByteClub" desc="Guys, I'm just here to tell you all about why the Morph Set is arguably the best armor set in all of Wynncraft. First of all, you get at least 70 skill points in both agility and defense, which is far more survivability than most mainstream builds can claim to have. Some other perks of the Morph Set are its 6/4 mana regen, very high health (13.5k), and 110% loot and XP bonus. The loot and XP bonus are extremely helpful, as it gives morph a use in XP grinding, such as Glowbulb Cave parties and CUR parties. It can also be used during lootrunning. These uses are further complemented by the set's high survivability, making it the best choice for cost-effective grinding. It also comes with -1 cost in every spell and a minimum of 70 intelligence, meaning that even the most costly spells, like Aura, are only 3 mana. Other builds without reduced spell costs would spec at least 94 skill points into intelligence to achieve the same effect, while with Morph you only need to spec 31 skill points into intelligence. Thats a 67% decrease! Finally, the most valuable trait of the Morph Set is its low cost. Morph Sets can be acquired for as little as 1LE for the entire set! Other builds need several LE for a single item, or even stacks of LE if they are using mythics! I hope that the information I have brought here today has inspired you to use the Morph Set on your next class, or to use it when you get to level 100. Thank you for reading.
-" img="main/demo2.png" logo="" ip="byteclub.minehut.gg" />
+              <ServerCard title="ByteClub" desc=":amoongus:" img="main/demo2.png" logo="" ip="byteclub.minehut.gg" />
               
             </div>
-            <h2 className="pb-[10000px]">Overly sized div to test sizing</h2>
+            
           {/*footer here*/}
         </div>
     </>
