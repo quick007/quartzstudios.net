96142ee5bbcfda2607d7705a347cac415d655519
LukasmanMHdude <69743171+quick007@users.noreply.github.com>
2021-04-21 14:46:41 -0700

    cards :flushed:

--- components/servercard.tsx
+++ components/servercard.tsx
@@ -1,28 +1,27 @@
 import React from "react";
 import Link from "next/link";
+import { ArrowRightIcon } from "@heroicons/react/outline";
 
 const ServerCard = (props) => {
     return (
         <>
-            <div className="flex bg-gray-900 shadow-lg w-full lg:w-96 mt-5 lg:mt-0 border-b-2 border-red-600">
+            <div className="flex bg-gray-900 shadow-lg w-full lg:w-96 mt-5 lg:mt-0 border-b-2 border-red-600 ">
                 <ul>
                     <li className="relative">
                         <img src={props.img} className=""/>
                         <div className="absolute inset-0 bg-gradient-to-b from-[rgba(0,0,0,1)] via-[rgba(0,0,0,0)] to-gray-900" />
                         <img className="absolute left-[10px] top-[10px] w-5/12 " src={props.logo} />
                     </li>
-                    <li className="m-4">
-                        <h2 className="prose-2xl text-white font-semibold">{props.title}</h2>
-                        <p className="prose-lg mb-2 text-gray-200 mx-1">{props.desc}</p>
+                    <li className="m-3">
+                        <h2 className="prose-2xl text-white font-semibold mx-1">{props.title}</h2>
+                        <p className="prose-lg text-gray-200 mx-2">{truncate(props.desc,25)}</p>
+                        <h5 className="rounded-full select-none px-2 prose text-gray-200 opacity-60 hover:opacity-90 font-semibold inline-flex hover:bg-gray-700 cursor-pointer mb-5">Read More <ArrowRightIcon className=" ml-2 h-7 w-5" /></h5>
                         <div className="relative">
-                            <div className={"rounded-full px-2 prose text-gray-200 font-semibold inline-flex bg-red-700"}>Offline</div>
-                            <button 
-                            className="group absolute right-0 inline-flex text-gray-200 rounded-full px-2 bg-gray-800 hover:bg-gray-700 focus:bg-green-600 cursor-pointer font-roboto focus:outline-none"
-                            
-                            >
-                                <div className="relative items-center justify-center">
-                                    <span  className="group-hover:invisible group-focus:invisible">{props.ip}</span>
-                                    <span className="hidden group-hover:block group-focus:hidden absolute top-0 ">Click to copy</span>
+                            <div className={"rounded-full select-none px-2 prose text-gray-200 font-semibold inline-flex cursor-not-allowed bg-red-700"}>Offline</div>
+                            <button className="group absolute right-0 inline-flex text-gray-200 rounded-full px-2 bg-gray-800 hover:bg-gray-700 focus:bg-green-600 cursor-pointer font-roboto focus:outline-none">
+                                <div className="relative items-center justify-center select-none">
+                                    <span className="group-hover:invisible group-focus:invisible">{props.ip}</span>
+                                    <span className="text-center hidden group-hover:block group-focus:hidden absolute top-0 ">Click to copy</span>
                                     <span className="hidden group-focus:block absolute top-0 ">Copied!</span>
                                 </div>
                             </button>
@@ -37,3 +36,8 @@ const ServerCard = (props) => {
 }
 
 export default ServerCard;
+
+function truncate(str, n) {
+    return str.length > n ? str.substr(0, n) + "..." : str;
+  }
+  
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <>
         <Navbar title="Hello World" />
           <div className="bg-gray-700 flex-grow overflow-y-auto">
-            t
+            
             <div className="grid-rows-3 grid-flow-col gap-4 lg:space-y-0 lg:flex lg:flex-row px-6 w-full">
               <ServerCard title="Dawn Horizens" desc="foo" img="main/demo1.png" logo="logos/dh.png" ip="dev.baezor.com" />
               <ServerCard title="ByteClub" desc=":amoongus:" img="main/demo2.png" logo="" ip="byteclub.minehut.gg" />
