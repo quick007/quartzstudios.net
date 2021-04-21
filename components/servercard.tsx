753df6cd95cf2a43e6de71d3c398ae587509b841
LukasmanMHdude <69743171+quick007@users.noreply.github.com>
2021-04-21 16:31:24 -0700

    finish cards

--- components/servercard.tsx
+++ components/servercard.tsx
@@ -7,18 +7,26 @@ const ServerCard = (props) => {
         <>
             <div className="flex bg-gray-900 shadow-lg w-full lg:w-96 mt-5 lg:mt-0 border-b-2 border-red-600 ">
                 <ul>
-                    <li className="relative">
+                    <li className="relative lg:w-96">
                         <img src={props.img} className=""/>
                         <div className="absolute inset-0 bg-gradient-to-b from-[rgba(0,0,0,1)] via-[rgba(0,0,0,0)] to-gray-900" />
                         <img className="absolute left-[10px] top-[10px] w-5/12 " src={props.logo} />
                     </li>
                     <li className="m-3">
-                        <h2 className="prose-2xl text-white font-semibold mx-1">{props.title}</h2>
-                        <p className="prose-lg text-gray-200 mx-2">{truncate(props.desc,25)}</p>
-                        <h5 className="rounded-full select-none px-2 prose text-gray-200 opacity-60 hover:opacity-90 font-semibold inline-flex hover:bg-gray-700 cursor-pointer mb-5">Read More <ArrowRightIcon className=" ml-2 h-7 w-5" /></h5>
+                        <h2 className="prose-2xl text-white font-semibold mx-1">{props.name}</h2>
+                        <p className="prose-lg text-gray-200 mx-2 break-words h-16">{truncate(props.desc,75)}</p>
+                        <Link href={"/" + props.name}>
+                            <h5 className="group rounded-full select-none px-2 prose text-gray-200 opacity-60 hover:opacity-90 font-semibold inline-flex hover:bg-gray-700 cursor-pointer mb-5">
+                                Read More 
+                                <ArrowRightIcon className="ml-2 group-hover:ml-3 h-7 w-5" />
+                            </h5>
+                        </Link>
                         <div className="relative">
                             <div className={"rounded-full select-none px-2 prose text-gray-200 font-semibold inline-flex cursor-not-allowed bg-red-700"}>Offline</div>
-                            <button className="group absolute right-0 inline-flex text-gray-200 rounded-full px-2 bg-gray-800 hover:bg-gray-700 focus:bg-green-600 cursor-pointer font-roboto focus:outline-none">
+                            <button 
+                                onClick={() => navigator.clipboard.writeText(props.ip)} 
+                                className="group absolute right-0 inline-flex text-gray-200 rounded-full px-2 bg-gray-800 hover:bg-gray-700 focus:bg-green-600 cursor-pointer font-roboto focus:outline-none"
+                            >
                                 <div className="relative items-center justify-center select-none">
                                     <span className="group-hover:invisible group-focus:invisible">{props.ip}</span>
                                     <span className="text-center hidden group-hover:block group-focus:hidden absolute top-0 ">Click to copy</span>
--- pages/index.tsx
+++ pages/index.tsx
@@ -9,8 +9,8 @@ export default function Home() {
           <div className="bg-gray-700 flex-grow overflow-y-auto">
             
             <div className="grid-rows-3 grid-flow-col gap-4 lg:space-y-0 lg:flex lg:flex-row px-6 w-full">
-              <ServerCard title="Dawn Horizens" desc="foo" img="main/demo1.png" logo="logos/dh.png" ip="dev.baezor.com" />
-              <ServerCard title="ByteClub" desc=":amoongus:" img="main/demo2.png" logo="" ip="byteclub.minehut.gg" />
+              <ServerCard name="Dawn Horizens" desc=" G G G G G G G G G G G G G G G G G G G G G G G G G G G G G G G G G G G G G " img="main/demo1.png" logo="logos/dh.png" ip="dev.baezor.com" />
+              <ServerCard name="ByteClub" desc=":amooooooooooooooooooongus: When the imposter is suusssy" img="main/demo2.png" logo="" ip="byteclub.minehut.gg" />
               
             </div>
             
