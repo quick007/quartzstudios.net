b56e23ecf1f722a8ed5851b4fb52fb370b74c1b5
LukasmanMHdude <69743171+quick007@users.noreply.github.com>
2021-04-21 16:48:15 -0700

    cards aren't finished btw

--- components/servercard.tsx
+++ components/servercard.tsx
@@ -7,11 +7,13 @@ const ServerCard = (props) => {
         <>
             <div className="flex bg-gray-900 shadow-lg w-full lg:w-96 mt-5 lg:mt-0 border-b-2 border-red-600 ">
                 <ul>
+                    {/*Image*/}
                     <li className="relative lg:w-96">
                         <img src={props.img} className=""/>
                         <div className="absolute inset-0 bg-gradient-to-b from-[rgba(0,0,0,1)] via-[rgba(0,0,0,0)] to-gray-900" />
                         <img className="absolute left-[10px] top-[10px] w-5/12 " src={props.logo} />
                     </li>
+                    {/*Card Content*/}
                     <li className="m-3">
                         <h2 className="prose-2xl text-white font-semibold mx-1">{props.name}</h2>
                         <p className="prose-lg text-gray-200 mx-2 break-words h-16">{truncate(props.desc,75)}</p>
@@ -22,7 +24,9 @@ const ServerCard = (props) => {
                             </h5>
                         </Link>
                         <div className="relative">
+                            {/*Player counter/offline*/}
                             <div className={"rounded-full select-none px-2 prose text-gray-200 font-semibold inline-flex cursor-not-allowed bg-red-700"}>Offline</div>
+                            {/*Click to copy*/}
                             <button 
                                 onClick={() => navigator.clipboard.writeText(props.ip)} 
                                 className="group absolute right-0 inline-flex text-gray-200 rounded-full px-2 bg-gray-800 hover:bg-gray-700 focus:bg-green-600 cursor-pointer font-roboto focus:outline-none"
@@ -45,7 +49,8 @@ const ServerCard = (props) => {
 
 export default ServerCard;
 
-function truncate(str, n) {
-    return str.length > n ? str.substr(0, n) + "..." : str;
-  }
+//Cut a string off at a set value
+function truncate(str: string, maxLength: number, suffix = "..."): string {
+    return str.length > maxLength ? str.substr(-1, maxLength) + suffix : str;
+}
   
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 
 module.exports = {
   mode: "jit",
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  purge: ["./pages/**/*.{tsx}", "./components/**/*.{tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
