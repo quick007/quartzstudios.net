6735cd34f78b74ae868c3db88734733180247be0
LukasmanMHdude <69743171+quick007@users.noreply.github.com>
2021-04-22 14:59:37 -0700

    :kekw:

--- components/servercard.tsx
+++ components/servercard.tsx
@@ -5,18 +5,21 @@ import { ArrowRightIcon } from "@heroicons/react/outline";
 const ServerCard = (props) => {
     return (
         <>
-            <div className="flex bg-gray-900 shadow-lg w-full lg:w-96 mt-5 lg:mt-0 border-b-2 border-red-600 lg:mr-4">
+            <div className="flex bg-gray-900 shadow-lg w-full lg:w-96 border-b-2 border-red-600">
                 <ul>
                     {/*Image*/}
                     <li className="relative lg:w-96">
                         <img src={props.img} className=""/>
-                        <div className="absolute inset-0 bg-gradient-to-b from-[rgba(0,0,0,1)] via-[rgba(0,0,0,0)] to-gray-900" />
+                        <div className="absolute inset-0 bottom-[-1px] bg-gradient-to-b from-[rgba(0,0,0,1)] via-[rgba(0,0,0,0)] to-gray-900" />
                         <img className="absolute left-[10px] top-[10px] w-5/12 " src={props.logo} />
                     </li>
                     {/*Card Content*/}
                     <li className="m-3">
                         <h2 className="prose-2xl text-white font-semibold mx-1">{props.name}</h2>
-                        <p className="prose-lg text-gray-200 mx-2 break-words h-16">{truncate(props.desc,75)}</p>
+                        <p className="prose-lg text-gray-200 mx-2 break-words h-16">
+                            <span className="hidden lg:block">{truncate(props.desc,75)}</span>
+                            <span className="block lg:hidden">{truncate(props.desc,30)}</span>
+                        </p>
                         <Link href={"/" + props.name}>
                             <h5 className="group rounded-full select-none px-2 prose text-gray-200 opacity-60 hover:opacity-90 font-semibold inline-flex hover:bg-gray-700 cursor-pointer mb-5">
                                 Read More 
@@ -51,6 +54,6 @@ export default ServerCard;
 
 //Cut a string off at a set value
 function truncate(str: string, maxLength: number, suffix = "..."): string {
-    return str.length > maxLength ? str.substr(-1, maxLength) + suffix : str;
+    return str.length > maxLength ? str.substr(0, maxLength) + suffix : str;
 }
   
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,11 @@ export default function Home() {
   return (
     <>
         <Navbar title="Hello World" />
-          <div className="bg-gray-700 flex-grow overflow-y-auto">
+          <div className="bg-gray-700 flex-grow overflow-y-auto max-width-2xl">
+            <div className="relative">
+              <img src="main/demo3.png" className="w-full h-[calc(100vh+100px)]" />
+              <div className="absolute inset-0 bg-gradient-to-b from-[rgba(0,0,0,0)] via-[rgba(0,0,0,0)] to-gray-700 h-[calc(100vh+100px)]" />
+            
           {/*<Carousel itemsToShow={1} showArrows={false} enableAutoPlay={true} autoPlaySpeed={5000} isRTL={true}>
             <div>1</div>
             <div>2</div>
@@ -17,12 +21,19 @@ export default function Home() {
             <div>5</div>
             <div>6</div>
           </Carousel>*/}
-            <div className="grid-rows-3 grid-flow-col lg:flex lg:flex-row px-6 w-full">
-              <ServerCard name="Dawn Horizons" desc="poggersrow" img="/main/demo1.png" logo="logos/dh.png" ip="dev.baezor.com" />
-              <ServerCard name="ByteClub" desc=":amooooooooooooooooooongus: When the imposter is suusssy" img="/main/demo2.png" logo="" ip="byteclub.minehut.gg" />
-              
+              <div className="flex items-center justify-center">
+                <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-10"> {/*lg:absolute lg:bottom-[-600px]*/}
+                  <ServerCard name="Dawn Horizons" desc="poggersrow" img="/main/demo1.png" logo="logos/dh.png" ip="dev.baezor.com" />
+                  <ServerCard name="ByteClub" desc=":amoooooooooooongus: When the imposter is suusssy" img="/main/demo2.png" logo="" ip="byteclub.minehut.gg" />
+                  <ServerCard name="Dawn Horizons" desc="poggersrow" img="/main/demo1.png" logo="logos/dh.png" ip="dev.baezor.com" />
+                  <ServerCard name="Dawn Horizons" desc="poggersrow" img="/main/demo1.png" logo="logos/dh.png" ip="dev.baezor.com" />
+                  <ServerCard name="Dawn Horizons" desc="poggersrow" img="/main/demo1.png" logo="logos/dh.png" ip="dev.baezor.com" />
+                  <ServerCard name="Dawn Horizons" desc="poggersrow" img="/main/demo1.png" logo="logos/dh.png" ip="dev.baezor.com" />
+                  <ServerCard name="Dawn Horizons" desc="poggersrow" img="/main/demo1.png" logo="logos/dh.png" ip="dev.baezor.com" />
+                  <ServerCard name="Dawn Horizons" desc="poggersrow" img="/main/demo1.png" logo="logos/dh.png" ip="dev.baezor.com" />
+                </div>
+              </div>
             </div>
-            
           {/*footer here*/}
         </div>
     </>
