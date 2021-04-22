ac0436c35b9c70c2e2c90d9a6b755b0248ac3052
Lukas <69743171+quick007@users.noreply.github.com>
2021-04-21 19:53:22 -0700

    fix website not showing css

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ export default function Home() {
           <div className="bg-gray-700 flex-grow overflow-y-auto">
             
             <div className="grid-rows-3 grid-flow-col gap-4 lg:space-y-0 lg:flex lg:flex-row px-6 w-full">
-              <ServerCard name="Dawn Horizens" desc=" G G G G G G G G G G G G G G G G G G G G G G G G G G G G G G G G G G G G G " img="main/demo1.png" logo="logos/dh.png" ip="dev.baezor.com" />
+              <ServerCard name="Dawn Horizons" desc="poggersrow" img="main/demo1.png" logo="logos/dh.png" ip="dev.baezor.com" />
               <ServerCard name="ByteClub" desc=":amooooooooooooooooooongus: When the imposter is suusssy" img="main/demo2.png" logo="" ip="byteclub.minehut.gg" />
               
             </div>
