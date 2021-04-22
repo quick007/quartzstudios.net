56c06ea3de2d2cb9308e1c26058949924de6e36e
LukasmanMHdude <69743171+quick007@users.noreply.github.com>
2021-04-22 09:11:58 -0700

    t

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,25 @@
+
 import Navbar from "../components/navbar";
 import ServerCard from "../components/servercard";
-import { ArrowCircleRightIcon } from "@heroicons/react/outline";
+import Carousel from 'react-elastic-carousel';
+
 
 export default function Home() {
   return (
     <>
         <Navbar title="Hello World" />
           <div className="bg-gray-700 flex-grow overflow-y-auto">
-            
+          {/*<Carousel itemsToShow={1} showArrows={false} enableAutoPlay={true} autoPlaySpeed={5000} isRTL={true}>
+            <div>1</div>
+            <div>2</div>
+            <div>3</div>
+            <div>4</div>
+            <div>5</div>
+            <div>6</div>
+          </Carousel>*/}
             <div className="grid-rows-3 grid-flow-col gap-4 lg:space-y-0 lg:flex lg:flex-row px-6 w-full">
-              <ServerCard name="Dawn Horizons" desc="poggersrow" img="main/demo1.png" logo="logos/dh.png" ip="dev.baezor.com" />
-              <ServerCard name="ByteClub" desc=":amooooooooooooooooooongus: When the imposter is suusssy" img="main/demo2.png" logo="" ip="byteclub.minehut.gg" />
+              <ServerCard name="Dawn Horizons" desc="poggersrow" img="/main/demo1.png" logo="logos/dh.png" ip="dev.baezor.com" />
+              <ServerCard name="ByteClub" desc=":amooooooooooooooooooongus: When the imposter is suusssy" img="/main/demo2.png" logo="" ip="byteclub.minehut.gg" />
               
             </div>
             
@@ -19,6 +28,7 @@ export default function Home() {
     </>
   );
 }
+
 
 
 //'https://api.mcsrvstat.us/2/dev.baezor.com'
