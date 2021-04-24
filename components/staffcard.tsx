8a8d6731b4efcb90e6d0d10777d7866cf43f7104
Lukas <69743171+quick007@users.noreply.github.com>
2021-04-24 09:31:48 -0700

    Update staffcard.tsx

--- components/staffcard.tsx
+++ components/staffcard.tsx
@@ -9,7 +9,7 @@ const StaffCard = (props) => {
         <>
             <div className="flex">
                 <ul className="space-y-2">
-                    <img src={"https://crafatar.com/avatars/" + props.uuid} className="rounded mx-auto w-1/2" />
+                    <img src={"https://crafatar.com/avatars/" + props.uuid + "?overlay"} className="rounded mx-auto w-1/2" />
                     <h2 className={"flex justify-center font-quartz uppercase text-xs text-gray-200 rounded-sm tracking-tight px-1 bg-[" + props.roleColor +"]"}>{props.roleName}</h2>
                     <h2 className="flex justify-center font-roboto text-gray-200">{props.ign}</h2>
                 </ul>
