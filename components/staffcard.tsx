2873f6a0136ab624a3629fb57d237282c877e725
LukasmanMHdude <69743171+quick007@users.noreply.github.com>
2021-06-02 09:46:31 -0700

    center text

--- components/staffcard.tsx
+++ components/staffcard.tsx
@@ -10,7 +10,7 @@ const StaffCard = (props) => {
             <div className="flex flex-col">
                 <img src={"https://crafatar.com/avatars/" + props.uuid + "?overlay"} className="rounded mx-auto w-1/2" alt={props.ign + "'s minecraft skin head"} />
                 <h2 className={"flex justify-center font-quartz uppercase text-xs text-white rounded-sm tracking-tight mt-3 px-1 " + props.roleColor}>{props.roleName}</h2>
-                <h2 className="flex justify-center font-roboto mt-1 text-gray-200">{props.ign}</h2>
+                <h2 className="flex justify-center font-roboto mt-1 text-gray-200 text-center">{props.ign}</h2>
             </div>
         </>
     );
